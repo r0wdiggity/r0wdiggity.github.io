65e4cac97648c9730296d446768be115ea6423f8
Anthony Giovannelli <anthonygiovannelli@PS-US-0083.local>
2022-05-04 12:34:23 -0500

    added lbs logic

--- js/coggan.js
+++ js/coggan.js
@@ -1,12 +1,16 @@
 import init, {calculate_power} from "./rust.js";
 
-export function get_power_data(weight, threshold, five_min, one_min, five_sec, gender){
-    weight = parseFloat(document.forms[0].elements["weight"].value)
-    threshold = parseFloat(document.forms[0].elements["threshold"].value)
-    five_min = parseFloat(document.forms[0].elements["five_min"].value)
-    one_min = parseFloat(document.forms[0].elements["one_min"].value)
-    five_sec = parseFloat(document.forms[0].elements["five_sec"].value)
-    gender = document.forms[0].elements["gender"].value == 'male'
+export function get_power_data(){
+    let units = document.forms[0].elements["units"].value;
+    let weight;
+    if (units === "lbs") {
+        weight = parseFloat(document.forms[0].elements["weight"].value) * 0.4536;
+    } else { weight = parseFloat(document.forms[0].elements["weight"].value); }
+    let threshold = parseFloat(document.forms[0].elements["threshold"].value)
+    let five_min = parseFloat(document.forms[0].elements["five_min"].value)
+    let one_min = parseFloat(document.forms[0].elements["one_min"].value)
+    let five_sec = parseFloat(document.forms[0].elements["five_sec"].value)
+    let gender = document.forms[0].elements["gender"].value == 'male'
     init()
         .then(() => {
             let power_numbers = (calculate_power(weight, threshold, five_min, one_min, five_sec, gender));
